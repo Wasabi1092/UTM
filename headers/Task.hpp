5035d4ba86a709db3f947f9fbc16ef9cc31ea435
Wasabi1092 <xavier.tang109@gmail.com>
2025-10-05 17:32:34 +1030

    add tasks class

---
+++ headers/Task.hpp
@@ -0,0 +1,35 @@
+#ifndef TASK_HPP
+#define TASK_HPP
+
+#include <iostream>
+#include <ctime>
+
+struct tm datetime;
+
+class Task
+{
+    private:
+        int id;
+        static int next;
+        std::string name;
+        time_t start;
+        time_t end;
+        std::string location;
+        std::string description;
+    public:
+        Task()
+        {
+            next ++;
+            id = next;
+            name = "Task " + std::to_string(id);
+            time(&start);
+            time(&end);
+            location = "";
+            description = "";
+        }
+        bool edit(int id, std::string flag, std::string value);
+};
+
+int Task::next = 0;
+
+#endif
