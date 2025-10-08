6c22b4f022aca9eab1808679c332e84aa7abdee4
Wasabi1092 <xavier.tang109@gmail.com>
2025-10-09 09:52:57 +1030

    feat: add parsetime util function, add more functionality to openfile

--- headers/Task.hpp
+++ headers/Task.hpp
@@ -31,28 +31,27 @@ class Task
 {
     private:
     	int id;
-    	static int next;
     	std::string name;
     	time_t start;
     	time_t end;
-    	std::string location;
-    	std::string description;
+    	string location;
+        string description;
+        string subject;
 
     	// adding status and priority levels
     	Status status;
     	Priority priority;
 
     public:
-    	Task()
+    	Task(int num)
     	{
-    		next++;
-    		id = next;
+    		id = num;
     		name = "Task " + std::to_string(id);
     		time(&start);
     		time(&end);
     		location = "";
     		description = "";
-
+            subject = "";
     		// adding status and priority by default
     		status = Status::pending;
     		priority = Priority::medium;
@@ -87,18 +86,75 @@ class Task
     				  << "\n"
     				  << "Priority: " << priorityString(priority) << "\n\n";
     	}
-        void openFile()
+        void openFile(string flag)
         {
-            string cmd = "nano ~/.cache/foo.txt";
+            string exist;
+            if (flag == "--desc")
+            {
+                exist = description;
+            }
+            else if (flag == "--loc")
+            {
+                exist = location;
+            }
+            else if (flag == "--sub")
+            {
+                exist = subject;
+            }
+            else if (flag == "--start-time")
+            {
+                datetime = *localtime(&start);
+                exist = to_string(datetime.tm_mday) + "/" + to_string(datetime.tm_mon) + "/" + to_string(datetime.tm_year) + " " + to_string(datetime.tm_hour) + ":" + to_string(datetime.tm_min);
+            }
+            else if (flag == "--end-time")
+            {
+                datetime = *localtime(&start);
+                exist = to_string(datetime.tm_mday) + "/" + to_string(datetime.tm_mon) + "/" + to_string(datetime.tm_year) + " " + to_string(datetime.tm_hour) + ":" + to_string(datetime.tm_min);
+            }
+            string cmd = "touch ~/.cache/foo.txt";
             int res = system(cmd.c_str());
+            if (res != 0)
+            {
+                cout << "An error occurred when creating the file" << endl;
+            }
+
+
+            string home = getenv("HOME");
+            ofstream file( home + "/.cache/foo.txt");
+            file << exist;
+            file.close();
+            cmd = "nano ~/.cache/foo.txt";
+            res = system(cmd.c_str());
             if (res != 0)
             {
                 cout << "An error occurred trying to open the text editor" << endl;
                 return;
             }
         }
+        time_t parseTime(string result)
+        {
+            time_t time;
+
+            // expected format is dd/mm/yyyy hh:mm
+            int index = result.find(" ");
+            string date = result.substr(0, index);
+            string time_string = result.substr(index +1, result.length()-index);
+
+            datetime.tm_mday = stoi(date.substr(0, 2));
+            datetime.tm_mon = stoi(date.substr(3, 2));
+            datetime.tm_year = stoi(date.substr(6, 4));
+
+            datetime.tm_hour = stoi(time_string.substr(0, 2));
+            datetime.tm_min = stoi(time_string.substr(3, 2));
+
+            datetime.tm_isdst = -1;
+
+            time = mktime(&datetime);
+            return time;
+        }
         int edit(int id, string flag)
         {
+            openFile(flag);
             string home = getenv("HOME");
             ifstream file( home + "/.cache/foo.txt");
             string input;
@@ -131,7 +187,5 @@ class Task
             return 0;
         }
 };
-
-int Task::next = 0;
 
 #endif
