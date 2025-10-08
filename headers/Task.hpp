06b0e379dc41e2feeae5e011d6b8a0c1e28c6400
Wasabi1092 <xavier.tang109@gmail.com>
2025-10-08 17:19:31 +1030

    feat: add edit tasks

--- headers/Task.hpp
+++ headers/Task.hpp
@@ -1,8 +1,12 @@
 #ifndef TASK_HPP
 #define TASK_HPP
 
-#include <ctime>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
 
 struct tm datetime;
 
@@ -23,66 +27,90 @@ enum class Priority
 
 class Task
 {
-private:
-	int id;
-	static int next;
-	std::string name;
-	time_t start;
-	time_t end;
-	std::string location;
-	std::string description;
+    private:
+    	int id;
+    	static int next;
+    	std::string name;
+    	time_t start;
+    	time_t end;
+    	std::string location;
+    	std::string description;
 
-	// adding status and priority levels
-	Status status;
-	Priority priority;
+    	// adding status and priority levels
+    	Status status;
+    	Priority priority;
 
-public:
-	Task()
-	{
-		next++;
-		id = next;
-		name = "Task " + std::to_string(id);
-		time(&start);
-		time(&end);
-		location = "";
-		description = "";
+    public:
+    	Task()
+    	{
+    		next++;
+    		id = next;
+    		name = "Task " + std::to_string(id);
+    		time(&start);
+    		time(&end);
+    		location = "";
+    		description = "";
 
-		// adding status and priority by default
-		status = Status::pending;
-		priority = Priority::medium;
-	}
+    		// adding status and priority by default
+    		status = Status::pending;
+    		priority = Priority::medium;
+    	}
 
-	// getters and setters for status and priority
-	Status getStatus() const { return status; }
-	Priority getPriority() const { return priority; }
-	void setStatus(Status s) { status = s; }
-	void setPriority(Priority p) { priority = p; }
-	std::string priorityString(Priority p)
-	{
-		switch (p)
-		{
-		case Priority::high:
-			return "High";
-		case Priority::medium:
-			return "Medium";
-		case Priority::low:
-			return "Low";
-		}
-		return "Unknown";
-	}
+    	// getters and setters for status and priority
+    	Status getStatus() const { return status; }
+    	Priority getPriority() const { return priority; }
+    	void setStatus(Status s) { status = s; }
+    	void setPriority(Priority p) { priority = p; }
+    	std::string priorityString(Priority p)
+    	{
+    		switch (p)
+    		{
+    		case Priority::high:
+    			return "High";
+    		case Priority::medium:
+    			return "Medium";
+    		case Priority::low:
+    			return "Low";
+    		}
+    		return "Unknown";
+    	}
 
-	// print task
-	void printTask()
-	{
-		std::cout << "Task ID: " << id << "\n"
-				  << "Name: " << name << "\n"
-				  << "Status: "
-				  << (status == Status::pending ? "Pending" : "Completed")
-				  << "\n"
-				  << "Priority: " << priorityString(priority) << "\n\n";
-	}
-
-	bool edit(int id, std::string flag, std::string value);
+    	// print task
+    	void printTask()
+    	{
+    		std::cout << "Task ID: " << id << "\n"
+    				  << "Name: " << name << "\n"
+    				  << "Status: "
+    				  << (status == Status::pending ? "Pending" : "Completed")
+    				  << "\n"
+    				  << "Priority: " << priorityString(priority) << "\n\n";
+    	}
+        void openFile()
+        {
+            string cmd = "nano ~/.cache/foo.txt";
+            int res = system(cmd.c_str());
+            if (res != 0)
+            {
+                cout << "An error occurred trying to open the text editor" << endl;
+                return;
+            }
+        }
+        int edit(int id, string flag)
+        {
+            openFile();
+            ifstream file("~/.cache/foo.txt");
+            string input;
+            string result = "";
+            std::cout << file.is_open() << std::endl;
+            while (getline(file, input))
+            {
+                cout << input << endl;
+                result += input;
+            }
+            cout << result << endl;
+            file.close();
+            return 0;
+        }
 };
 
 int Task::next = 0;
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< HEAD
 #include <string>
 #include <sstream>
 
@@ -35,4 +36,15 @@ int main(int argc, char* argv[]) {
   tasksList.listAllTasks();
 
   return 0;
+=======
+#include "headers/Task.hpp"
+
+int main(int argc, char* argv[])
+{
+    // argc is number of args
+    // argv are the args passed in as a vector
+    Task task;
+    task.edit(0, "e");
+    return 0;
+>>>>>>> 6fb67b4 (feat: add edit tasks)
 }
